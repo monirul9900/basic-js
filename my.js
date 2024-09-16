08ba99ff3c55dc3a616475dcfca6fbcc8e7f462d
monirul <bdmonirulislam6@gmail.com>
2024-09-16 17:38:22 +0600

    test event function

--- my.js
+++ my.js
@@ -6,16 +6,24 @@ const birth_year = document.querySelector("input[placeholder ='birth year']");
 const click = document.querySelector("button");
 const h1 = document.querySelector("h1");
 
-click.addEventListener("click", function () {
-  if (!name.value || !birth_year.value) {
-    h1.innerHTML = "all fields are require";
-  } else {
-    const age = new Date().getFullYear() - Number(birth_year.value);
+// click.addEventListener("click", function () {
+//   if (!name.value || !birth_year.value) {
+//     h1.innerHTML = "all fields are require";
+//   } else {
+//     const age = new Date().getFullYear() - Number(birth_year.value);
 
-    if (age >= 21) {
-      h1.innerHTML = `<img src="https://media.tenor.com/q8TigR5GnWkAAAAM/firecracker-diwali.gif" alt="">`;
-    } else {
-      h1.innerHTML = `<img src="https://i.pinimg.com/originals/80/eb/ff/80ebff5d4895501127835eec2df88dd5.gif" alt="">`;
-    }
-  }
+//     if (age >= 21) {
+//       h1.innerHTML = `<img src="https://media.tenor.com/q8TigR5GnWkAAAAM/firecracker-diwali.gif" alt="">`;
+//     } else {
+//       h1.innerHTML = `<img src="https://i.pinimg.com/originals/80/eb/ff/80ebff5d4895501127835eec2df88dd5.gif" alt="">`;
+//     }
+//   }
+// });
+const sobji = document.querySelectorAll(".sobji ul li");
+let color = ["red", "green", "purpal", "blue"];
+
+sobji.forEach((item, index) => {
+  item.onclick = (event) => {
+    event.target.style.color = color[index];
+  };
 });
